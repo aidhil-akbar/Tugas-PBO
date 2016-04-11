d88677fa4cb54223c64174b07edbd1e136c3056d
aidhil akbar <aidhil akbar>
2016-04-12 02:40:14 +0700

    Tugas PBO

---
+++ src/Induk.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Induk {
+    String nama;
+     double jrs,td,pkl,fsk,ak;
+    
+    public void hitung(){
+    
+    
+        double hasil = (jrs+td+pkl+fsk)/4;
+    
+    }
+    
+    
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getjrs() {
+        return jrs;
+    }
+
+    public void setjrs(double jrs) {
+        this.jrs = jrs;
+    }
+
+    public double gettd() {
+        return td;
+    }
+
+    public void settd(double td) {
+        this.td = td;
+    }
+
+    public double getpkl() {
+        return pkl;
+    }
+
+    public void setpkl(double pkl) {
+        this.pkl = pkl;
+    }
+
+    public double getfsk() {
+        return fsk;
+    }
+
+    public void setfsk(double fsk) {
+        this.fsk = fsk;
+    }
+
+    public double getAkir() {
+        return ak;
+    }
+
+    public void setAkir(double akir) {
+        this.ak = akir;
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/Peranakan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Peranakan extends Induk{
+    
+    //perubahan sifat
+   public void hitung(){
+   
+   ak = (jrs + td + pkl + fsk) /4;
+   
+   }
+
+ 
+  
+    
+    
+}
